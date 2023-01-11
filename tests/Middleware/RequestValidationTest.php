b1af383f933218f2ac13e10431d43b5f6a74426c
John Charman <john.charman@imhotek.net>
2023-01-11 20:58:20 +0000

    improve RequestValidation tests

--- tests/Middleware/RequestValidationTest.php
+++ tests/Middleware/RequestValidationTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Membrane\Psr15\Middleware;
 
+use Illuminate\Http\Response;
+use Membrane\OpenAPI\Exception\CannotProcessRequest;
+use Membrane\OpenAPI\Method;
 use Membrane\Psr15\ApiProblemBuilder;
 use Membrane\Psr15\Container;
 use Membrane\Result\Result;
@@ -42,10 +45,31 @@ class RequestValidationTest extends TestCase
 
         $sut->process($request, self::createStub(RequestHandlerInterface::class));
     }
-    /** @test */
-    public function catchesCannotProcessRequest(): void
+
+    public function dataSetsThatThrowCannotProcessRequest(): array
     {
-        $request = new ServerRequest('get', '/hats');
+        return [
+            'path not found' => [
+                '/hats',
+                Method::GET,
+                CannotProcessRequest::pathNotFound('petstore-expanded.json', '/hats'),
+            ],
+            'method not found' => [
+                '/pets',
+                Method::DELETE,
+                CannotProcessRequest::methodNotFound(Method::DELETE->value),
+            ],
+            // TODO test 406 from unsupported content-types once Membrane is reading content-types from requests
+        ];
+    }
+
+
+    /**
+     * @test
+     * @dataProvider dataSetsThatThrowCannotProcessRequest
+     */
+    public function catchesCannotProcessRequest(string $path, Method $method, CannotProcessRequest $expected): void
+    {
         $apiProblemBuilder = self::createMock(ApiProblemBuilder::class);
         $sut = new RequestValidation(
             __DIR__ . '/../fixtures/petstore-expanded.json',
@@ -55,8 +79,9 @@ class RequestValidationTest extends TestCase
 
         $apiProblemBuilder
             ->expects($this->once())
-            ->method('buildFromException');
+            ->method('buildFromException')
+            ->with($expected);
 
-        $sut->process($request, self::createStub(RequestHandlerInterface::class));
+        $sut->process(new ServerRequest($method->value, $path), self::createStub(RequestHandlerInterface::class));
     }
 }
